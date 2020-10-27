4f085f216da2cf0c4711c1e7e66cc1a1353a6ec5
uriart <loiro_8@hotmail.com>
2020-10-27 20:37:00 +0100

    primer commit

---
+++ src/app/services/deseos.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Lista } from '../models/lista.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DeseosService {
+
+  listas: Lista[] = [];
+
+  constructor() { 
+    
+    const lista1 = new Lista('Recolectar piedras del infinito');
+    const lista2 = new Lista('Heroes a desaparecer');
+
+    this.listas.push(lista1, lista2);
+
+  }
+
+  
+  crearLista( titulo: string ){
+    const nuevaLista = new Lista(titulo);
+    this.listas.push( nuevaLista );
+  }
+
+}
